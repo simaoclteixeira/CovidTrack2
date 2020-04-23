560b96e98fec6e3aceff2a7cbc0539a421d14407
simaoclteixeira <sclteixeira11@gmail.com>
2020-04-23 19:06:53 +0100

    Começo de criação de interfaces

---
+++ app/src/main/java/pt/ipg/covidtrack2/ProfilesActivity.java
@@ -0,0 +1,14 @@
+package pt.ipg.covidtrack2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profiles);
+    }
+}
